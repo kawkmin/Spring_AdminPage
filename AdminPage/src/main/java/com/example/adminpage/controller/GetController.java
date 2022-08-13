ce5a3acac61ede152515036d73e46ed7db706425
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-08-13 14:53:00 +0900

    Controller, Mapping

---
+++ AdminPage/src/main/java/com/example/adminpage/controller/GetController.java
@@ -0,0 +1,35 @@
+package com.example.adminpage.controller;
+
+import com.example.adminpage.moel.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api") //Localhost:8080/api
+public class GetController {
+
+    @RequestMapping(method = RequestMethod.GET,path="/getMethod")//Localhost:8080/api/getMethod
+    public String getRequest(){
+
+        return "Hi getMethod";
+    }
+
+    @GetMapping("/getParameter") //Localhost:8080/api/getParameter?id=1234&password=abcd
+    public String getParameter(@RequestParam String id, @RequestParam(name="password") String pwd){ //name지정
+        System.out.println("id :"+id);
+        System.out.println("password:" +pwd);
+
+        return id+pwd;
+    }
+
+    //localhost:8080/api/multiParameter?account=abcd&email=study@gmail.com&page=10
+    @GetMapping("getMultiParameter")
+    public SearchParam getMultiParameter(SearchParam searchParam){ //타입을 객체로 메소드 선언시 JSON 형식 자동 바꿈
+        System.out.println(searchParam.getAccount());
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+
+        //{"account": "","email" : "","page" :0}
+        return searchParam;
+    }
+
+}
---
+++ AdminPage/src/main/java/com/example/adminpage/controller/PostController.java
@@ -0,0 +1,29 @@
+package com.example.adminpage.controller;
+
+import com.example.adminpage.moel.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+
+    //html <Form>
+    //ajax 검색
+    //http post body ->data
+    //json,xml,multipart-form/text-plain
+
+
+    @PostMapping(value = "/postMethod")
+    public SearchParam postMethod(@RequestBody SearchParam searchParam){
+        return searchParam;
+    }
+
+    @PutMapping
+    public void put(){
+
+    }
+    @PatchMapping
+    public void Patch(){
+
+    }
+}
