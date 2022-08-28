648b480454188f875f621210c52ac71eae733640
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-08-28 15:47:14 +0900

    Controller Abstarct적용

--- AdminPage/src/main/java/com/example/adminpage/controller/api/ItemApiController.java
+++ AdminPage/src/main/java/com/example/adminpage/controller/api/ItemApiController.java
@@ -1,41 +1,25 @@
 package com.example.adminpage.controller.api;
 
-import com.example.adminpage.ifs.CrudInterface;
-import com.example.adminpage.moel.network.Header;
+import com.example.adminpage.controller.CrudController;
 import com.example.adminpage.moel.network.request.ItemApiRequest;
 import com.example.adminpage.moel.network.response.ItemApiResponse;
 import com.example.adminpage.service.ItemApiLogicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/api/item")
-public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+public class ItemApiController extends CrudController<ItemApiRequest, ItemApiResponse> {
 
     @Autowired
     private ItemApiLogicService itemApiLogicService;
 
-    @Override
-    @PostMapping("")
-    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.create(request);
+    @PostConstruct
+    public void init(){
+        this.baseService=itemApiLogicService;
     }
 
-    @Override
-    @GetMapping("{id}")
-    public Header<ItemApiResponse> read(@PathVariable Long id) {
-        return itemApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("")
-    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")
-    public Header delete(@PathVariable Long id) {
-        return itemApiLogicService.delete(id);
-    }
 }
--- AdminPage/src/main/java/com/example/adminpage/controller/api/OrderGroupApiController.java
+++ AdminPage/src/main/java/com/example/adminpage/controller/api/OrderGroupApiController.java
@@ -1,41 +1,21 @@
 package com.example.adminpage.controller.api;
 
-import com.example.adminpage.ifs.CrudInterface;
-import com.example.adminpage.moel.network.Header;
+import com.example.adminpage.controller.CrudController;
 import com.example.adminpage.moel.network.request.OrderGroupApiRequest;
 import com.example.adminpage.moel.network.response.OrderGroupApiResponse;
 import com.example.adminpage.service.OrderGroupApiLogicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/orderGroup")
-public class OrderGroupApiController implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+public class OrderGroupApiController extends CrudController<OrderGroupApiRequest, OrderGroupApiResponse> {
 
     @Autowired
     private OrderGroupApiLogicService orderGroupApiLogicService;
 
-    @Override
-    @PostMapping("")
-    public Header<OrderGroupApiResponse> create(@RequestBody Header<OrderGroupApiRequest> request) {
-        return orderGroupApiLogicService.create(request);
-    }
-
-    @Override
-    @GetMapping("{id}")
-    public Header<OrderGroupApiResponse> read(@PathVariable Long id) {
-        return orderGroupApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("")
-    public Header<OrderGroupApiResponse> update(@RequestBody Header<OrderGroupApiRequest> request) {
-        return orderGroupApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")
-    public Header delete(@PathVariable Long id) {
-        return orderGroupApiLogicService.delete(id);
+    public void init(){
+        this.baseService=orderGroupApiLogicService;
     }
 }
